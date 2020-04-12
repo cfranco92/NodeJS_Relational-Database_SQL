78d01e6931d5ee118aafe7b098e0d62fe299596e
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2020-04-12 14:26:55 -0500

    Separating the posts to a microservice.

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
 const errors = require('../network/errors');
 
 const app = express();
@@ -18,11 +17,10 @@ const swaggerDoc = require('./swagger.json');
 // ROUTER
 app.use('/api/user', user);
 app.use('/api/auth', auth);
-app.use('/api/post', post);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(errors);
 
 app.listen(config.api.port, () => {
-    console.log('API LISTENING IN THE PORT ', config.api.port);
+    console.log('API LISTENING ON THE PORT ', config.api.port);
 });
